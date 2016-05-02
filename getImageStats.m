458b194c83c37a8082978a3c90f10e40cdfe032c
iker.llorens.13@gmail.com <ikerllorens@Ikers-MacBook-Pro.local>
2016-05-02 12:53:16 -0500

    Primera Red Neuronal con Globos, Canguro y Caracol

---
+++ getImageStats.m
@@ -0,0 +1,37 @@
+function [STATS] = getImageStats(Im)
+%Im = imread('proyecto110.jpg');
+Im = rgb2gray(Im);
+H = fspecial('average');
+Im = imfilter(Im,H);
+
+[~, threshold] = edge(Im, 'prewitt');
+fudgeFactor = .3;
+BW_edge = edge(Im,'prewitt', threshold * fudgeFactor);
+
+se90 = strel('line', 2, 90);
+se0 = strel('line', 2, 0);
+
+BW_dil = imdilate(BW_edge, [se90 se0]);
+
+BW_nobord = imclearborder(BW_dil,4);
+
+BW_nobord = bwareaopen(BW_nobord, 50,4);
+
+BW_fill = imfill(BW_nobord, 'holes');
+
+
+subplot(1,2,1),imshow(Im,[]);
+subplot(1,2,2),imshow(BW_fill,[]);
+
+[L,Num] = bwlabel(BW_fill,8);
+
+STATS = regionprops(L, 'Area', 'MajorAxisLength', 'MinorAxisLength', 'Eccentricity', 'ConvexArea' ...
+    , 'EquivDiameter', 'Perimeter', 'Solidity', 'Extent');
+
+STATS = struct2cell(STATS);
+STATS = cell2mat(STATS);
+%area_s = regionprops(L,'area');
+% area = struct2cell(area_s);
+
+% resultados = ['c', 'o', 'p' ,'c', 'm'];
+end
