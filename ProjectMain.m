b205e58eff2cec0b41653a86603020bd547b2ddf
iker.llorens.13@gmail.com <ikerllorens@Ikers-MacBook-Pro.local>
2016-05-02 16:41:55 -0500

    No se Necesitan Palabras -_-

---
+++ ProjectMain.m
@@ -0,0 +1,9 @@
+%% Proyecto Final Procesamiento Digital de Im?genes 2016
+   % Iker Llorens
+   % Marco Antonio Marquina
+   % Andres Velasco
+   
+   
+%% Lectura de im?genes
+
+images = getImages();
