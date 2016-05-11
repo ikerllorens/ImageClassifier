6bf748a847dc3ea977fe74e33f17bbb97e467570
Iker Llorens <iker.llorens.13@gmail.com>
2016-05-11 13:57:05 -0500

    Project Cleaning

--- ProjectMain.m
+++
@@ -1,9 +0,0 @@
-%% Proyecto Final Procesamiento Digital de Im?genes 2016
-   % Iker Llorens
-   % Marco Antonio Marquina
-   % Andres Velasco
-   
-   
-%% Lectura de im?genes
-
-images = getImages();
